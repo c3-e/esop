b50f92870daad0d1360d66c39341103df58fb514
Amit Salunke <amit.salunke@c3iot.ai>
2020-11-25 12:10:17 -0800

    use env variables instead of k8s secrets

--- src/main/java/com/instaclustr/esop/s3/TransferManagerFactory.java
+++ src/main/java/com/instaclustr/esop/s3/TransferManagerFactory.java
@@ -1,17 +1,8 @@
 package com.instaclustr.esop.s3;
 
-import static com.google.common.base.Strings.isNullOrEmpty;
-import static com.instaclustr.kubernetes.KubernetesHelper.isRunningAsClient;
-import static java.lang.String.format;
-
-import java.util.Map;
-
 import com.amazonaws.ClientConfiguration;
 import com.amazonaws.Protocol;
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
-import com.amazonaws.auth.WebIdentityTokenCredentialsProvider;
 import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
@@ -20,9 +11,6 @@ import com.amazonaws.services.s3.transfer.TransferManager;
 import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
 import com.google.inject.Provider;
 import com.instaclustr.esop.impl.AbstractOperationRequest;
-import com.instaclustr.kubernetes.KubernetesHelper;
-import com.instaclustr.kubernetes.KubernetesSecretsReader;
-import com.instaclustr.kubernetes.SecretReader;
 import io.kubernetes.client.apis.CoreV1Api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -48,13 +36,8 @@ public class TransferManagerFactory {
         return TransferManagerBuilder.standard().withS3Client(amazonS3).build();
     }
 
-    public boolean isRunningInKubernetes() {
-        return KubernetesHelper.isRunningInKubernetes() || isRunningAsClient();
-    }
-
     protected AmazonS3 provideAmazonS3(final Provider<CoreV1Api> coreV1ApiProvider, final AbstractOperationRequest operationRequest) {
-
-        final S3Configuration s3Conf = resolveS3Configuration(coreV1ApiProvider, operationRequest);
+        final S3Configuration s3Conf = resolveS3ConfigurationFromEnvProperties();
 
         final AmazonS3ClientBuilder builder = AmazonS3ClientBuilder.standard();
 
@@ -113,96 +96,17 @@ public class TransferManagerFactory {
         return builder.build();
     }
 
-    protected S3Configuration resolveS3Configuration(final Provider<CoreV1Api> coreV1ApiProvider, final AbstractOperationRequest operationRequest) {
-        if (isRunningInKubernetes()) {
-            if (isNullOrEmpty(operationRequest.resolveKubernetesSecretName())) {
-                logger.warn("Kubernetes secret name for resolving S3 credentials was not specified, going to resolve them from env. properties. "
-                                + "If env. properties are not specified, credentials will be fetched from AWS instance itself.");
-                return resolveS3ConfigurationFromEnvProperties();
-            }
-            return resolveS3ConfigurationFromK8S(coreV1ApiProvider, operationRequest);
-        } else {
-            return resolveS3ConfigurationFromEnvProperties();
-        }
-    }
-
-    private S3Configuration resolveS3ConfigurationFromK8S(final Provider<CoreV1Api> coreV1ApiProvider, final AbstractOperationRequest operationRequest) {
-
-        final String secretName = operationRequest.resolveKubernetesSecretName();
-
-        try {
-            final String namespace = resolveKubernetesKeyspace(operationRequest);
-            final SecretReader secretReader = new SecretReader(coreV1ApiProvider);
-
-            return secretReader.readIntoObject(namespace,
-                                               secretName,
-                                               secret -> {
-                                                   final Map<String, byte[]> data = secret.getData();
-
-                                                   final S3Configuration s3Configuration = new S3Configuration();
-
-                                                   final byte[] awsendpoint = data.get("awsendpoint");
-                                                   final byte[] awsregion = data.get("awsregion");
-                                                   final byte[] awssecretaccesskey = data.get("awssecretaccesskey");
-                                                   final byte[] awsaccesskeyid = data.get("awsaccesskeyid");
-
-                                                   if (awsendpoint != null) {
-                                                       s3Configuration.awsEndpoint = new String(awsendpoint);
-                                                   }
-
-                                                   if (awsregion != null) {
-                                                       s3Configuration.awsRegion = new String(awsregion);
-                                                   }
-
-                                                   if (awsaccesskeyid != null) {
-                                                       s3Configuration.awsAccessKeyId = new String(awsaccesskeyid);
-                                                   } else {
-                                                       throw new S3ModuleException(format("Secret %s does not contain any entry with key 'awsaccesskeyid'.",
-                                                                                          secret.getMetadata().getName()));
-                                                   }
-
-                                                   if (awssecretaccesskey != null) {
-                                                       s3Configuration.awsSecretKey = new String(awssecretaccesskey);
-                                                   } else {
-                                                       throw new S3ModuleException(format("Secret %s does not contain any entry with key 'awssecretaccesskey'.",
-                                                                                          secret.getMetadata().getName()));
-                                                   }
-
-                                                   return s3Configuration;
-                                               });
-        } catch (final Exception ex) {
-            throw new S3ModuleException("Unable to resolve S3 credentials for backup / restores from Kubernetes secret " + secretName, ex);
-        }
-    }
-
     private S3Configuration resolveS3ConfigurationFromEnvProperties() {
         final S3Configuration s3Configuration = new S3Configuration();
 
         s3Configuration.awsRegion = System.getenv("AWS_REGION");
         s3Configuration.awsEndpoint = System.getenv("AWS_ENDPOINT");
-        s3Configuration.awsAccessKeyId = System.getenv("AWS_ACCESS_KEY_ID");
-        s3Configuration.awsSecretKey = System.getenv("AWS_SECRET_KEY");
-
-        // accesskeyid and awssecretkey will be taken from normal configuration mechanism in ~/.aws/ ...
-        // s3Configuration.awsAccessKeyId = System.getenv("AWS_ACCESS_KEY_ID");
-        // s3Configuration.awsSecretKey = System.getenv("AWS_SECRET_KEY");
 
         return s3Configuration;
     }
 
-    private String resolveKubernetesKeyspace(final AbstractOperationRequest operationRequest) {
-        if (operationRequest.resolveKubernetesNamespace() != null) {
-            return operationRequest.resolveKubernetesNamespace();
-        } else {
-            return KubernetesSecretsReader.readNamespace();
-        }
-    }
-
     public static final class S3Configuration {
-
         public String awsRegion;
         public String awsEndpoint;
-        public String awsAccessKeyId;
-        public String awsSecretKey;
     }
 }
